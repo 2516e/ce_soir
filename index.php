3ee0efe160d35070c4f98e3dd7b44b1f7b96851b
2516e <mahleb.enzo@gmail.com>
2024-06-10 16:52:37 +0200

    10/06/24 ce_soir

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <main>
         <section class="content">
-          <div id="scrollbutton">
+          <div class="firstcontent row">
             <img src="public/assets/img/asperge.jpg" alt="asperge" />
             <div class="text">
               <h1 id="our_story">our story</h1>
@@ -15,38 +15,37 @@
                 dolorem ab blanditiis hic soluta impedit perspiciatis magni
                 molestiae quae. Fugiat commodi voluptatem quam repellat.
               </p>
-              <h4>est.2004</h4>
+              <h5>est.2004</h5>
             </div>
           </div>
-          <div>
+          <div class="secondcontent row">
             <img src="public/assets/img/savon.jpg" alt="savon" />
             <div class="text">
-              <img src="public/assets/img/graines_01.jpg" alt="graine" />
+              <img src="public/assets/img/graines_01.jpg" alt="graine" class="textimg"/>
               <h1>fresh fare</h1>
               <hr class="content">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
                 facilis, magnam debitis enim magni nam.
               </p>
-              <h4>all natural</h4>
+              <h5>all natural</h5>
             </div>
           </div>
-          <div>
+          <div class="thirdcontent row">
             <img src="public/assets/img/moule.jpg" alt="" />
             <div class="text">
-              <img src="public/assets/img/graines_02.jpg" alt="grainebis" />
+              <img src="public/assets/img/graines_02.jpg" alt="grainebis" class="textimg"/>
               <h1>local distillation</h1>
               <hr class="content">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
                 incidunt!
               </p>
-              <h4>craft liquor</h4>
+              <h5>craft liquor</h5>
             </div>
           </div>
         </section>
         <section class="oeuf">
-          <img src="public/assets/img/oeuf.jpg" alt="oeuf" />
         </section>
       </main>
 <?php include __DIR__ . "/public/footer.html.php" ?>
--- public/footer.html.php
+++ public/footer.html.php
@@ -1,34 +1,39 @@
 <footer>
+  <div class="footerhero">
     <div class="left">
-        <h2 class="logotitle">
-        <div class="logo">
-            c<sup><u>e</u></sup> soir
-        </h2>
-          <hr class="cesoir" />
-        </div>
-          <p class="info">
-            3346 mio street
-            <br />
-            henryville, ca 77095
-          </p>
-          <p class="info">
-            tel (00) 123 456 789
-            <br />
-            tel (00) 123 456 789
-          </p>
-        </div>
-        <div class="socials">
-          <a href=""
-            ><i class="fa-brands fa-instagram fa-xs" style="color: #e53f6f"></i
-          ></a>
-          <a href=""
-            ><i class="fa-brands fa-facebook-f fa-xs" style="color: #e53f6f"></i
-          ></a>
-          <a href=""
-            ><i class="fa-brands fa-google fa-xs" style="color: #e53f6f"></i
-          ></a>
-          <a href=""
-            ><i class="fa-brands fa-twitter fa-xs" style="color: #e53f6f"></i
-          ></a>
-        </div>
+      <div class="logo">
+        <h1 class="logotitle">
+          <a href="./">c<sup><u>e</u></sup> soir</a>
+        </h1>
+        <hr class="cesoir" />
+      </div>
+      <br>
+      <br>
+        <p class="info">
+          3346 mio street
+          <br />
+          henryville, ca 77095
+        </p>
+        <br>
+        <p class="info">
+          tel (00) 123 456 789
+          <br />
+          tel (00) 123 456 789
+        </p>
+    </div>
+    <div class="socials">
+        <a href="">
+          <i class="fa-brands fa-instagram fa-lg" style="color: #e53f6f"></i>
+        </a>
+        <a href="">
+          <i class="fa-brands fa-facebook-f fa-lg" style="color: #e53f6f"></i>
+        </a>
+        <a href="">
+          <i class="fa-brands fa-google fa-lg" style="color: #e53f6f"></i>
+        </a>
+        <a href="">
+          <i class="fa-brands fa-twitter fa-lg" style="color: #e53f6f"></i>
+        </a>
+    </div>
+  </div>
 </footer>
